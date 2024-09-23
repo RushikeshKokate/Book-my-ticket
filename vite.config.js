2e2fa018c2426491fd2a0c8c7cb39a7ba609d02b
RushikeshKokate22 <rushikesh.kokate@tradebrains.in>
2024-09-23 15:40:52 +0530

    error fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  outDir: 'dist',
-})
+  build: {
+    rollupOptions: {
+      external: [], // Avoid externalizing react-redux
+    },
+  },
+});
