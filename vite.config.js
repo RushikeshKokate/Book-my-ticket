d46adba9eaff90b39b3813b51a610229039714f3
RushikeshKokate22 <rushikesh.kokate@tradebrains.in>
2024-09-23 16:04:30 +0530

    deployment error

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: [], // Avoid externalizing react-redux
-    },
+      external: ['react-redux', 'redux', 'react-router-dom']
+    }
   },
 });
